73aa204afa162e00d1922ebd05da07f83a3a17f5
taymitsu <tmitsumori96@gmail.com>
2023-12-06 19:42:02 -0800

    battle arena added

--- hero.py
+++ hero.py
@@ -4,6 +4,90 @@ from armor import Armor
 from weapon import Weapon
 from team import Team
 
+class Arena:
+  def __init__(self):
+    self.team_one = Team("Team One")
+    self.team_two = Team("Team Two")
+  # TODO: create instance variables named team_one and team_two that
+  # will hold our teams.
+
+  def create_ability(self):
+    name = input("What is the ability name?  ")
+    max_damage = int(input("What is the max damage of the ability?  "))
+    return Ability(name, max_damage)
+
+  def create_weapon(self):
+    name = input("What is the name of this weapon?  ")
+    max_damage = input("What is the max damage of this weapon?  ")
+    return Weapon(name, max_damage)
+
+  def create_armor(self):
+    name = input("What is the armor name? > ")
+    max_block = input("What is the max block of the armor? > ")
+    return Armor(name, max_block)
+
+  def create_hero(self):
+    hero_name = input("Hero's name: ")
+    hero = Hero(hero_name)
+    add_item = None
+
+    while add_item != "4":
+      add_item = input("[1] Add ability\n[2] Add weapon\n[3] Add armor\n[4] Done adding items\n\nYour choice: ")
+      if add_item == "1":
+        hero.add_ability(self.create_ability())
+      elif add_item == "2":
+        hero.add_weapon(self.create_weapon())
+      elif add_item == "3":
+        hero.add_armor(self.create_armor())
+    return hero
+
+  def build_team_one(self):
+    numOfTeamMembers = int(input("How many members would you like on Team One?\n"))
+    for i in range(numOfTeamMembers):
+      hero = self.create_hero()
+      self.team_one.add_hero(hero)
+
+  def build_team_two(self):
+    numOfTeamMembers = int(input("How many members would you like on Team Two?\n"))
+    for i in range(numOfTeamMembers):
+      hero = self.create_hero()
+      self.team_two.add_hero(hero)
+
+  def team_battle(self):
+      self.team_one.attack(self.team_two)
+
+  def show_stats(self):
+      print("\n")
+      print(self.team_one.name + " statistics: ")
+      self.team_one.stats()
+      print("\n")
+      print(self.team_two.name + " statistics: ")
+      self.team_two.stats()
+      print("\n")
+
+      team_kills_one = sum([hero.kills for hero in self.team_one.heroes])
+      team_deaths_one = sum([hero.deaths for hero in self.team_one.heroes])
+      if team_deaths_one == 0:
+        team_deaths_one = 1
+      print(self.team_one.name + " average K/D was: " + str(team_kills_one/team_deaths_one))
+
+      team_kills_two = sum([hero.kills for hero in self.team_two.heroes])
+      team_deaths_two= sum([hero.deaths for hero in self.team_two.heroes])
+      if team_deaths_two == 0:
+        team_deaths_two = 1
+      print(self.team_two.name + " average K/D was: " + str(team_kills_two/team_deaths_two))
+
+      print("Survived from " + self.team_one.name + ": ")
+      for hero in self.team_one.heroes:
+        if hero.deaths == 0:
+          print("- " + hero.name)
+
+      print("Survived from " + self.team_two.name + ": " + hero.name)
+      for hero in self.team_two.heroes:
+        if hero.deaths == 0:
+          print("- " + hero.name)
+          
+
 class Hero:
   def __init__(self, name, starting_health=100):
     self.name = name
@@ -86,7 +170,11 @@ class Hero:
 if __name__ == "__main__":
   # If you run this file from the terminal
   # this block is executed.
-
+  arena = Arena()
+  arena.build_team_one()
+  arena.build_team_two()
+  arena.team_battle()
+  arena.show_stats()
   hero1 = Hero("Wonder Woman")
   hero2 = Hero("Dumbledore")
   ability1 = Ability("Super Speed", 300)
--- weapon.py
+++ weapon.py
@@ -1,11 +1,12 @@
 from ability import Ability
 import random
 
-class Weapon(Ability):
+class Weapon:
+  def __init__(self, name, max_damage):
+    self.name = name
+    self.max_damage = int(max_damage)
+ 
   def attack(self):
-    """  This method returns a random value
-    between one half to the full attack power of the weapon.
-    """
     # TODO: Use integer division to find half of the max_damage value
     # then return a random integer between half of max_damage and max_damage
     random_value = random.randint(self.max_damage // 2, self.max_damage)
